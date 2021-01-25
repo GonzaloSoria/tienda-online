c9f929b7866f403eac7f09a6ce0d0f5ed79666ee
Gonzalo Soria <gonzalo.sooria@gmail.com>
2021-01-25 13:49:17 -0300

    Subida del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componentes/navbar/index';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
